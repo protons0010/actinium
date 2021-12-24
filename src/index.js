b55cf390727bf93f5de138029aa3c453cb3bd93d
Rahat Ahmed <71262853+protons0010@users.noreply.github.com>
2021-12-24 22:12:11 +0600

    commencement

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// npm install react-router-dom
+
+ReactDOM.render(<App />,document.getElementById('root')
+);
+
